07c5e926b0a69a86d550520049e7a1ba24913f04
Khushi Bera <145190251+khushi2758@users.noreply.github.com>
2025-03-10 02:15:54 +0530

    progressbar

--- src/component/Quiz.jsx
+++ src/component/Quiz.jsx
@@ -1,26 +1,50 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import QUESTIONS from "../questions.js";
+import quizIsCompleteImg from "../assets/quiz-complete.png";
+import QuestionTimer from "./QuestionTimer.jsx";
 export default function Quiz() {
   const [useAnswers, setUseAnswers] = useState([]);
   const activeQuestionIndex = useAnswers.length;
-  function handleSelectAnswer(selectedAnswer) {
+
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+  const handelSelectAnswer =useCallback( function handelSelectAnswer(selectedAnswer) {
     setUseAnswers((prevUserAnswer) => {
       return [...prevUserAnswer, selectedAnswer];
     });
+  },[]);
+  
+  const handleSkipAnswer = useCallback(()=> handelSelectAnswer(null),[handelSelectAnswer])
+
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizIsCompleteImg} alt=" Trophy icon" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
   }
+
+  const shuffledAnswer = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswer.sort((a, b) => Math.random() - 0.5);
   return (
-     <div id="quiz">
-    <div id="question">
-      {" "}
-      <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-      <ul id="answers">
-        {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
-          <li key={answer} className="answer">
-            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <div id="quiz">
+      <div id="question">
+        <QuestionTimer 
+        key={activeQuestionIndex}
+        timeout={10000} 
+        onTimeout={handleSkipAnswer}/>
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {shuffledAnswer.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handelSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
